e718811de3128b9afa868dd2645b605d618cdde6
LuciaNishimiya <lucianishimiya@gmail.com>
2024-04-20 16:00:00 -0400

    Dia 8 Contar Palabras

---
+++ dia-8/index.js
@@ -0,0 +1,29 @@
+
+/*
+ * Crea un programa que cuente cuantas veces se repite cada palabra
+ * y que muestre el recuento final de todas ellas.
+ * - Los signos de puntuación no forman parte de la palabra.
+ * - Una palabra es la misma aunque aparezca en mayúsculas y minúsculas.
+ * - No se pueden utilizar funciones propias del lenguaje que
+ *   lo resuelvan automáticamente.
+ */
+
+
+function contarPalabras(texto) {
+    const palabras = texto.toLowerCase().replace(/[.,;'"()]/g, '').split(' ');
+    let recuento = {};
+
+    palabras.forEach(palabra => {
+        if (recuento[palabra]) {
+            recuento[palabra] = recuento[palabra] + 1;
+        } else {
+            recuento[palabra] = 1;
+        }
+    });
+
+    return recuento;
+}
+
+const miTexto = 'En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lentejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda. El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de lo mesmo, y los días de entresemana se honraba con su vellorí de lo más fino. Tenía en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que así ensillaba el rocín como tomaba la podadera.'
+
+console.table(contarPalabras(miTexto));
