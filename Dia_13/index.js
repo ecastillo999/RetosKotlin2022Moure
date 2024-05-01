17a2c3001b1d3ee9f64296da07cf68c1da54fba0
unknown <gerardo_duarte@live.com>
2024-04-30 21:00:55 -0300

    Dia 13

---
+++ Dia_13/index.js
@@ -0,0 +1,16 @@
+function esPalidromo(texto){
+    texto = texto.replaceAll(/[ -,-.]/g, "").toLowerCase();
+    texto = texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+    let index = texto.length
+    for(caracter in texto){
+        if(texto[caracter] !== texto[index-1]){
+            console.log(texto[caracter],"=",texto[index-1])
+            return false
+        }
+        index--
+    }
+    return true;
+}
+
+console.log(esPalidromo("Ana lleva al oso la avellana."))
+console.log(esPalidromo("Ella, te dará detalle."))
